1effd61d61df582ba32d1201fbd3e4b705ed90f6
iTrooz <hey@itrooz.fr>
2024-06-14 11:04:39 +0200

    start to add logic code

--- main.go
+++ main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Manifest struct {
+	Name            string `json:"name"`
+	ShortName       string `json:"short_name"`
+	StartURL        string `json:"start_url"`
+	Icons           []Icon `json:"icons"`
+	Display         string `json:"display"`
+	ThemeColor      string `json:"theme_color"`
+	BackgroundColor string `json:"background_color"`
+}
+
+type Icon struct {
+	Src   string `json:"src"`
+	Sizes string `json:"sizes"`
+	Type  string `json:"type"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/manifest.json", manifestHandler)
+	http.HandleFunc("/icon.png", iconHandler)
+	http.HandleFunc("/", rootHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("Server listening on port %s...\n", port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func manifestHandler(w http.ResponseWriter, r *http.Request) {
+	manifest := Manifest{
+		Name:      "My PWA",
+		ShortName: "PWA",
+		StartURL:  "/",
+		Icons: []Icon{
+			{
+				Src:   "/icon.png",
+				Sizes: "192x192",
+				Type:  "image/png",
+			},
+		},
+		Display:         "standalone",
+		ThemeColor:      "#ffffff",
+		BackgroundColor: "#ffffff",
+	}
+
+	manifestBytes, err := json.Marshal(manifest)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(manifestBytes)
+}
+
+func iconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "icon.jpg")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+	if url == "" {
+		http.ServeFile(w, r, "index.html")
+	} else {
+		http.Redirect(w, r, url, http.StatusMovedPermanently)
+	}
 }
