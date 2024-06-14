33d26650694dffea5db14cb03c29f69f53699cc3
iTrooz <hey@itrooz.fr>
2024-06-14 12:01:09 +0200

    add logging middleware

--- main.go
+++ main.go
@@ -37,7 +37,27 @@ func main() {
 		port = "8080"
 	}
 	fmt.Printf("Server listening on port %s...\n", port)
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(":"+port, loggingMiddleware(http.DefaultServeMux))
+}
+
+// see https://gist.github.com/nstogner/2d6e122418ad3e21a175974e5c9bb36c
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rec := statusRecorder{w, 200}
+		next.ServeHTTP(&rec, r)
+		println(rec.status)
+		fmt.Printf("%v %v %v\n", rec.status, r.Method, r.URL.Path)
+	})
 }
 
 type UserManifestData struct {
