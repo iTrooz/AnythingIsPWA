8c6b3e640e8ad654c9b8f346bce02da429007016
iTrooz <hey@itrooz.fr>
2024-11-24 00:09:09 +0100

    add flag to enable verbose mode

--- internal/intmain/intmain.go
+++ internal/intmain/intmain.go
@@ -7,7 +7,19 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func Main() {
+func parseCLI() {
+	verbose := false
+	if len(os.Args) > 1 && os.Args[1] == "-v" {
+		verbose = true
+	}
+
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	logrus.Debug("Verbose logging enabled")
+}
+
+func setupWebServer(port string) {
 	http.HandleFunc("/{$}", rootHandler)
 	http.HandleFunc("/app", appHandler)
 	http.HandleFunc("/app/manifest.json", manifestHandler)
@@ -19,14 +31,21 @@ func Main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	logrus.Infof("Server listening on port %s...", port)
+	logrus.Infof("Server listening on port %v...", port)
 
 	err := http.ListenAndServe(":"+port, loggingMiddleware(http.DefaultServeMux))
 	if err != nil {
 		logrus.Fatalf("Failed to start server: %v", err)
 	}
+}
+
+func Main() {
+	parseCLI()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	setupWebServer(port)
 }
