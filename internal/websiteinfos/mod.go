f2ee646803941c715914afcee4ca3f49d44acfaa
iTrooz <hey@itrooz.fr>
2024-11-23 20:05:34 +0100

    refactor icon code in icon.go

--- internal/websiteinfos/mod.go
+++ internal/websiteinfos/mod.go
@@ -5,11 +5,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"slices"
-	"strconv"
-	"strings"
 
-	"github.com/sirupsen/logrus"
 	"golang.org/x/net/html"
 )
 
@@ -57,114 +53,6 @@ func verifyURLIsSafe(url_str string) error {
 	return nil
 
 }
-
-type icon struct {
-	height int
-	width  int
-	link   string
-}
-
-func (i *icon) AnySize() bool {
-	return i.height == 0 && i.width == 0
-}
-
-func sizeScore(height, width int) int {
-	return height * width
-}
-
-func parseSize(size string) (int, int) {
-	split := strings.Split(size, "x")
-	if len(split) != 2 {
-		logrus.Warnf("Found invalid size property: %v", size)
-		return 0, 0
-	}
-
-	height, err := strconv.Atoi(split[0])
-	if err != nil {
-		logrus.Warnf("Failed to parse height: %v", err)
-		return 0, 0
-	}
-
-	width, err := strconv.Atoi(split[1])
-	if err != nil {
-		logrus.Warnf("Failed to parse width: %v", err)
-		return 0, 0
-	}
-
-	return height, width
-}
-
-// Construct a icon struct that represents the best size this icon can have
-func constructIconStruct(base_link, rel_link string, sizes string) *icon {
-	link, err := url.JoinPath(base_link, rel_link)
-	if err != nil {
-		logrus.Warnf("Failed to join URL path: %v", err)
-		return nil
-	}
-
-	currentIcon := icon{}
-
-	for _, size := range strings.Split(sizes, " ") {
-		if size == "any" {
-			return &icon{
-				link: link,
-			}
-		}
-
-		height, width := parseSize(size)
-		if height == 0 && width == 0 {
-			continue
-		}
-
-		if sizeScore(height, width) > sizeScore(currentIcon.height, currentIcon.width) {
-			currentIcon = icon{
-				height: height,
-				width:  width,
-				link:   link,
-			}
-		}
-	}
-
-	if currentIcon.height == 0 && currentIcon.width == 0 {
-		return nil
-	} else {
-		return &currentIcon
-	}
-}
-
-func tryFindIcon(str_url string, n *html.Node) *icon {
-	// Search for basic icons
-	if n.Type == html.ElementNode && n.Data == "link" {
-		for _, attr := range n.Attr {
-			// Look for rel
-			if attr.Key != "rel" {
-				continue
-			}
-
-			// check if icon or apple-touch-icon
-			values := strings.Split(attr.Val, " ")
-			if !slices.Contains(values, "icon") && !slices.Contains(values, "apple-touch-icon") {
-				continue
-			}
-
-			href := getAttr(n, "href")
-			sizes := getAttr(n, "sizes")
-			if href == nil || sizes == nil {
-				logrus.Warningf("Found icon without href or sizes: %v", nodeToString(n))
-				continue
-			}
-
-			// Extract icon
-			icon := constructIconStruct(str_url, href.Val, sizes.Val)
-			if icon != nil {
-				return icon
-			}
-		}
-	}
-
-	return nil
-}
-
 func Get(str_url string) (*WebsiteInfos, error) {
 	// verify URL
 	err := verifyURLIsSafe(str_url)
